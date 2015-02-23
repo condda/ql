74d2e74859f3ee93141ac4956e3dfb898be9760e
Jonatan <felipez@live.nl>
2015-02-23 12:00:04 +0100

    Initial Commit

---
+++ QuestionnaireLanguage/AST/Test/TestClass.cs
@@ -0,0 +1,49 @@
+﻿using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+using AST.Nodes.Interfaces;
+using AST.ParseTreeVisitors;
+using Grammar;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AST.Test
+{
+    public class TestClass
+    {
+        public TestClass()
+        {
+
+        }
+
+        public IASTNode GetAST(string path)
+        {
+            string program = File.ReadAllText(path);
+
+            AntlrInputStream input = new AntlrInputStream(program);
+            QLMainLexer lexer = new QLMainLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            QLMainParser parser = new QLMainParser(tokens);
+
+            //remove standard Error Listener
+            parser.RemoveErrorListeners();
+
+            //Detect more errors (like ambiguousness errors)
+            //parser.AddErrorListener(new DiagnosticErrorListener());
+
+            //add standard error listener
+            parser.AddErrorListener(new ParserErrorListener());
+
+            IParseTree tree = parser.form();
+
+            Console.WriteLine(tree.ToStringTree(parser));
+            MainVisitor visitor = new MainVisitor();
+            IASTNode ast = visitor.Visit(tree);
+
+            return ast;
+        }
+    }
+}
